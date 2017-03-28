f3b5f62d2d60c1d88cd8ec8111d8b8dd390cbd87
Martin Coleman <Martinc94@live.co.uk>
2017-03-28 15:18:13 +0100

    reset variable on recconnect

--- MyoSnake/MyoSnake/Classes/MyoManager.cs
+++ MyoSnake/MyoSnake/Classes/MyoManager.cs
@@ -18,9 +18,9 @@ namespace MyoSnake.Classes
         private static MyoManager myoManager = new MyoManager();
 
         public bool UseMyo = true;
+        public bool isConnected = true;
 
         IChannel _myoChannel;
-        IChannel _myoChannel1;
         IHub _myoHub;
        // IHub _myoHub1;
 
@@ -47,24 +47,26 @@ namespace MyoSnake.Classes
         public void connect()
         { // communication, device, exceptions, poses
 
+            IChannel _myoChannel=null;
+            IHub _myoHub=null;
             try
             {
+                playerName.Clear();
+                playerId.Clear();
+                MyoEventArgsList.Clear();
+
                 // create the channel
                 _myoChannel = Channel.Create(ChannelDriver.Create(ChannelBridge.Create(), MyoErrorHandlerDriver.Create(MyoErrorHandlerBridge.Create())));
 
                 // create the hub with the channel
                 _myoHub = MyoSharp.Device.Hub.Create(_myoChannel);
-
+            
                 // create the event handlers for connect and disconnect
                 _myoHub.MyoConnected += _myoHub_MyoConnected;
                 _myoHub.MyoDisconnected += _myoHub_MyoDisconnected;
 
                 // start listening 
                 _myoChannel.StartListening();
-
-                // create the channel
-                _myoChannel1 = Channel.Create(ChannelDriver.Create(ChannelBridge.Create(),
-                                        MyoErrorHandlerDriver.Create(MyoErrorHandlerBridge.Create())));
 
             }
             catch (System.InvalidOperationException e)
@@ -99,7 +101,7 @@ namespace MyoSnake.Classes
 
             if (playerId.Count == 2)
             {
-                for (int i = 0; i < 2; i++)
+                for (int i = 1; i < 2; i++)
                 {
                     playerName.Add(playerId[i], "Player" + (i + 1));
                 }
@@ -119,6 +121,23 @@ namespace MyoSnake.Classes
             string pName = "";
             playerName.TryGetValue(handle, out pName);
             return pName;
+        }
+
+        public void Vibrate(string name)
+        {      
+            foreach (var myo in MyoEventArgsList)
+            {
+
+                string pName = "";
+                playerName.TryGetValue(myo.Myo.Handle.ToString(), out pName);
+
+                if (pName==name) {
+                    myo.Myo.Vibrate(VibrationType.Medium);
+                }
+            }
+
+
+           
         }
 
     } // class
